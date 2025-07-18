fa5382ea10a03b1f0c71306142bf94bfde2e52a0
chinnukush <chinnukushal123@gmail.com>
2025-07-18 21:31:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# Startup Config
+API_ID = "15671595"
+API_HASH = "bb8f36f9c39a24c7f8b2acbc7ea8c60a"
+BOT_TOKEN = "7390174586:AAGlfMP3iN70hjpHAfB8YcUna5tDgpLXC4w"
+AUTH_CHANNEL = "-1002723124765"
+DATABASE = "mongodb+srv://fyviodemo:XKr72sW6QOzDAU8Y@cluster0.5y1fiek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+TMDB_API = "imdb.kyt43843.workers.dev"
+IMDB_API = ""
+
+# SERVER
+BASE_URL = "https://grim-robenia-koyebuser123-05b315bb.koyeb.app"  
+PORT = "8000"
+
+# Update
+UPSTREAM_REPO = "https://github.com/chinnukush/backend"
+UPSTREAM_BRANCH = "main"
+
+# Additional CDN Bots
+# MULTI_TOKEN1 = ""
+
+USE_CAPTION = "True"
+USE_TMDB = "True"
+OWNER_ID = "7253187871"
