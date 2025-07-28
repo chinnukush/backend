f3095d668ca4aee218bd016a8e6069cf7e9aa42d
chinnukush <chinnukushal123@gmail.com>
2025-07-28 11:11:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 # Startup Config
 API_ID = "15671595"
 API_HASH = "bb8f36f9c39a24c7f8b2acbc7ea8c60a"
-BOT_TOKEN = "7390174586:AAGlfMP3iN70hjpHAfB8YcUna5tDgpLXC4w"
+BOT_TOKEN = "7313932976:AAFnh1bszQItH6U3bZAwCXwXj58m2AxyhcM"
 AUTH_CHANNEL = "-1002723124765"
 DATABASE = "mongodb+srv://fyviodemo:XKr72sW6QOzDAU8Y@cluster0.5y1fiek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-TMDB_API = ""
+TMDB_API = "6abcb6bb99fb77f33c37016a28866ed2"
 IMDB_API = "https://imdb-api.pandapelisv.workers.dev/"
 
 # SERVER
