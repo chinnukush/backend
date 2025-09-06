83eee05c6c6ba4e372e1869bfff584d3c74d2b6e
chinnukush <chinnukushal123@gmail.com>
2025-09-06 19:33:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,23 @@
 # Startup Config
 API_ID = "15671595"
 API_HASH = "bb8f36f9c39a24c7f8b2acbc7ea8c60a"
-BOT_TOKEN = "7390174586:AAGlfMP3iN70hjpHAfB8YcUna5tDgpLXC4w"
+BOT_TOKEN = "7313932976:AAFnh1bszQItH6U3bZAwCXwXj58m2AxyhcM"
 AUTH_CHANNEL = "-1002723124765"
-DATABASE = "mongodb+srv://fyviodemo:XKr72sW6QOzDAU8Y@cluster0.5y1fiek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE = "mongodb+srv://anikush8310_db_user:t2tx8gNKqrdqvmsh@cluster0.chhz1qg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 TMDB_API = "6abcb6bb99fb77f33c37016a28866ed2"
-IMDB_API = ""
+IMDB_API = "https://imdb-api.pandapelisv.workers.dev"
 
 # SERVER
 BASE_URL = "https://willing-bathsheba-koyebuser123-644c06c2.koyeb.app"  
 PORT = "8000"
 
 # Update
-UPSTREAM_REPO = "https://github.com/chinnukush/backend"
+UPSTREAM_REPO = "https://github.com/Kushalhk/Backend_bot"
 UPSTREAM_BRANCH = "main"
 
 # Additional CDN Bots
 # MULTI_TOKEN1 = "7390174586:AAGlfMP3iN70hjpHAfB8YcUna5tDgpLXC4w"
 
 USE_CAPTION = "True"
-USE_TMDB = "False"
+USE_TMDB = "True"
 OWNER_ID = "7253187871"
